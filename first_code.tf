773b930dbb9fbb2f4d52c5fbaf273516de44ab1c
Max Conroy <max.conroy@outlook.com>
2022-02-09 11:34:46 -0500

    .tf edit

--- first_code.tf
+++ first_code.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile = "default"
-  region = "us-east-2"
+  region  = "us-east-2"
 }
 
 resource "aws_s3_bucket" "tf_course" {
-  bucket = "tf-course-2022"
-  acl = "private"
+  bucket = "tf-course-2022-02"
+  acl    = "private"
 }
